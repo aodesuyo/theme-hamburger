8cae143423a224e474d384f5bfbf7847effe64e8
aoaoaoaoao___________ <z.1a2oyk@gmail.com>
2021-06-02 07:02:28 +0900

    header.php タイトルタグとserchform.phpの作成

--- header.php
+++ header.php
@@ -1,28 +1,19 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta chars et="UTF-8">
+    <meta charset=<?php bloginfo( 'charset' )?>>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="./css/ress.css" rel="stylesheet">
-    <link href="./css/hamburger.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@500&display=swap" rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.0.0/css/all.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
-    <!-- <title>Document</title> -->
     <?php wp_head(); ?>
 </head>
 <body>
+    <?php wp_body_open(); ?>
     <div class="c-colum">
         <header class="l-header p-header">
             <button class="p-header__nav-btn">Menu</button>
             <div class="l-header-top">
-                <h1 class="p-header-top__title">Hamburger</h1>
-                <form class="p-header-top__search">
-                    <input type="search" name="search" for="search" class="font-awesome">
-                    <i class="fas fa-search fa-2x"></i>
-                    <input type="submit" name="submit" value="検索" id="search">
-                </form>                
+                <h1 class="p-header-top__title">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ) ?></a></h1>
+                <?php get_search_form() ?>
             </div>
         </header>
---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form class="p-header-top__search" method="get" action="<?php echo home_url('/'); ?>">
+    <input type="search" name="s" for="search" class="font-awesome" id="s">
+    <i class="fas fa-search fa-2x"></i>
+    <input type="submit" name="submit" value="検索" id="search">
+</form>                
